33472a01df21db425befae2a4a3effec87ec0120
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2021-12-19 21:03:27 -0300

    Agregar archivos de proyecto.

---
+++ Muestra del 10 al 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muestra_del_10_al_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
